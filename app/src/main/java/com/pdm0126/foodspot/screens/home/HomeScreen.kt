d2b38c9764d5239d4a635842e0675cc68494757d
acalidonio <116671236+acalidonio@users.noreply.github.com>
2026-05-16 03:52:40 -0600

    Feat: Añadir titulo principal, botón de busqueda y todos los tpitulos de categorías a la home screen

--- app/src/main/java/com/pdm0126/foodspot/screens/home/HomeScreen.kt
+++ app/src/main/java/com/pdm0126/foodspot/screens/home/HomeScreen.kt
@@ -1,8 +1,61 @@
 package com.pdm0126.foodspot.screens.home
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    onNavigateToDetail: (Int) -> Unit,
+    onNavigateToSearch: () -> Unit,
+    viewModel: HomeViewModel = viewModel()
+) {
+    val uiState by viewModel.uiState.collectAsState()
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        "FoodSpot",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
+                    ) },
+                actions = {
+                    IconButton(onNavigateToSearch) {
+                        Icon(imageVector = Icons.Default.Search, contentDescription = "Buscar")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            uiState.groupedRestaurants.forEach { (category, restaurants) ->
+                item {
+                    Text(
+                        text = category,
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary,
+                        modifier = Modifier.padding(start = 16.dp, top = 24.dp, bottom = 8.dp)
+                    )
+                }
+            }
+        }
+    }
 }
